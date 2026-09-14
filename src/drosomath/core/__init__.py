05ab70471be4f6c9c6b4ad22c1660db7cfa0ba32
alfred0105 <72288851+alfred0105@users.noreply.github.com>
2026-09-14 22:50:47 +0900

    Phase 4: export structural plasticity primitives

--- src/drosomath/core/__init__.py
+++ src/drosomath/core/__init__.py
@@ -1,9 +1,17 @@
 from .plasticity import PlasticityTracker, RewardWeightRule, SynapseUse
+from .structural import (
+    RewireResult,
+    StructuralPlasticityConfig,
+    StructuralPlasticityManager,
+)
 from .synapse import SynapseState
 
 __all__ = [
     "PlasticityTracker",
     "RewardWeightRule",
+    "RewireResult",
+    "StructuralPlasticityConfig",
+    "StructuralPlasticityManager",
     "SynapseState",
     "SynapseUse",
 ]
